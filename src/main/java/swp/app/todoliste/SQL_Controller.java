b09000cc8c1dc49fabd6450fd21d10d2f7356767
Werzu25 <leonw@astecs.com>
2024-10-21 08:50:30 +0200

    peter du fetter hurensohn

--- src/main/java/swp/app/todoliste/SQL_Controller.java
+++ src/main/java/swp/app/todoliste/SQL_Controller.java
@@ -6,12 +6,11 @@ import java.sql.SQLException;
 
 public class SQL_Controller {
 
-    private static Database db; //leons db
+    public static Connection connection = null;
     public static void initConnection(String url) {
         try {
-            db = new Database(url);
-        } catch (SQLException | ClassNotFoundException e) {
-            System.out.println("Connection to SQLite has not been established.");
+            connection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("VendorError: " + e.getErrorCode());
@@ -36,23 +35,5 @@ public class SQL_Controller {
     }
     public static void deleteEntry() {
 
-    }
-
-    public static void createDB(boolean insertTestData) {
-        try {
-            db.executeStatement("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, " +
-                    "task TEXT, " +
-                    "due_date TEXT, " +
-                    "priority INTEGER)");
-            if (insertTestData)// mehr spalten selber einfügen leon leck meine eier
-            db.executeStatement("INSERT INTO tasks (task, due_date, priority) VALUES " +
-                    "('Task 1', '2023-10-01', 1), " +
-                    "('Task 2', '2023-10-02', 2), " +
-                    "('Task 3', '2023-10-03', 3)");
-        } catch (SQLException e) {
-            System.out.println("SQLException: " + e.getMessage());
-            System.out.println("SQLState: " + e.getSQLState());
-            System.out.println("VendorError: " + e.getErrorCode());
-        }
     }
 }
