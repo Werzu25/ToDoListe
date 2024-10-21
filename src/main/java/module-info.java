a58e99f1df6ca9adb81142620cb5b3c55391daee
Peter Quickner <98043962+DevilsBestFriend@users.noreply.github.com>
2024-10-21 08:38:53 +0200

    leons

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module swp.app.todoliste {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens swp.app.todoliste to javafx.fxml;
-    exports swp.app.todoliste;
-}
--- src/main/java/swp/app/todoliste/SQL_Controller.java
+++ src/main/java/swp/app/todoliste/SQL_Controller.java
@@ -6,11 +6,12 @@ import java.sql.SQLException;
 
 public class SQL_Controller {
 
-    public static Connection connection = null;
+    private static Database db; //leons db
     public static void initConnection(String url) {
         try {
-            connection = DriverManager.getConnection(url);
-        } catch (SQLException e) {
+            db = new Database(url);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Connection to SQLite has not been established.");
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("VendorError: " + e.getErrorCode());
@@ -35,5 +36,18 @@ public class SQL_Controller {
     }
     public static void deleteEntry() {
 
+    }
+
+    public static void createDB(){
+        try {
+            db.executeStatement("CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, " +
+                    "task TEXT, " +
+                    "due_date TEXT, " +
+                    "priority INTEGER)"); // mehr spalten selber einfügen leon leck meine eier
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        }
     }
 }
